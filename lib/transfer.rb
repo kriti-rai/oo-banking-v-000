ff71a26221465af4a9616ce44f974f03ac9ad194
Kriti Rai <kriti.rai@gmail.com>
2018-02-13 17:15:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
 
   def execute_transaction
-      if !self.sender.valid?
+      if !self.sender.valid? || self.sender.balance < self.amount
         "Transaction rejected. Please check your account balance."
         self.status = "rejected"
 
